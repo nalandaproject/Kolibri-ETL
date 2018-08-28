d757dd7ef95b54a69978fed5fea6c7bfcbf5198d
yogeshmhaskule <yogesh.mhaskule@gmail.com>
2018-08-28 14:33:29 +0530

    Added code to fetch the exam/lesson details

--- Code/Fetcher.py
+++ Code/Fetcher.py
@@ -66,8 +66,8 @@ class Fetcher(object):
                     res = self.checkRecordExist(str(row[2]), EntityTable, sinkConnection, row)
                     if res != 0:      
                         stmt = update(EntityTable).where(EntityTable.c.id==str(row[2])).values({EntityTable.c.password: row[0],EntityTable.c.last_login: row[1], EntityTable.c._morango_dirty_bit: row[3], \
-                               EntityTable.c._morango_source_id:row[4], EntityTable.c._morango_partition:row[5], EntityTable.c.username: row[6], EntityTable.c.full_name:row[7], \
-                               EntityTable.c.date_joined:row[8], EntityTable.c.dataset_id:row[9], EntityTable.c.facility_id:row[10]})
+                               EntityTable.c._morango_source_id:row[4], EntityTable.c._morango_partition:row[5], EntityTable.c.username: row[10], EntityTable.c.full_name:row[6], \
+                               EntityTable.c.date_joined:row[7], EntityTable.c.dataset_id:row[8], EntityTable.c.facility_id:row[9]})
                         sinkConnection.execute(stmt)
         
                 elif str(EntityTable.name) == 'kolibriauth_collection':
@@ -75,8 +75,8 @@ class Fetcher(object):
                     if res != 0: 
                         # logging.info("inside else kolibriauth_collection") 
                         stmt = update(EntityTable).where(EntityTable.c.id==str(row[0])).values({EntityTable.c._morango_dirty_bit: row[1], \
-                               EntityTable.c._morango_source_id:row[2], EntityTable.c._morango_partition:row[3], EntityTable.c.name: row[4], EntityTable.c.kind:row[5], \
-                               EntityTable.c.lft:row[6], EntityTable.c.rght:row[7], EntityTable.c.tree_id:row[8], EntityTable.c.level:row[9], EntityTable.c.dataset_id:row[10], \
+                               EntityTable.c._morango_source_id:row[2], EntityTable.c._morango_partition:row[3], EntityTable.c.name: row[4], EntityTable.c.kind:row[10], \
+                               EntityTable.c.lft:row[5], EntityTable.c.rght:row[6], EntityTable.c.tree_id:row[7], EntityTable.c.level:row[8], EntityTable.c.dataset_id:row[9], \
                                EntityTable.c.parent_id:row[11]})
                         # logging.info(str(stmt))
                         sinkConnection.execute(stmt)  
@@ -86,20 +86,22 @@ class Fetcher(object):
                     if res != 0: 
                         # logging.info("inside else kolibriauth_role")
                         stmt = update(EntityTable).where(EntityTable.c.id==str(row[0])).values({EntityTable.c._morango_dirty_bit: row[1], \
-                               EntityTable.c._morango_source_id:row[2], EntityTable.c._morango_partition:row[3], EntityTable.c.kind: row[4], EntityTable.c.collection_id:row[5], \
-                               EntityTable.c.dataset_id:row[6], EntityTable.c.user_id:row[7]})
+                               EntityTable.c._morango_source_id:row[2], EntityTable.c._morango_partition:row[3], EntityTable.c.kind: row[7], EntityTable.c.collection_id:row[4], \
+                               EntityTable.c.dataset_id:row[5], EntityTable.c.user_id:row[6]})
                         # logging.info(str(stmt))
                         sinkConnection.execute(stmt)  
+
                 elif str(EntityTable.name) == 'logger_contentsessionlog':
                     res = self.checkRecordExist(str(row[0]), EntityTable, sinkConnection, row)
                     if res != 0: 
                         # logging.info("inside else kolibriauth_collection") 
                         stmt = update(EntityTable).where(EntityTable.c.id==str(row[0])).values({EntityTable.c._morango_dirty_bit: row[1], \
                                EntityTable.c._morango_source_id:row[2], EntityTable.c._morango_partition:row[3], EntityTable.c.content_id: row[4], EntityTable.c.channel_id:row[5], \
-                               EntityTable.c.start_timestamp:row[6], EntityTable.c.end_timestamp:row[7], EntityTable.c.time_spent:row[8], EntityTable.c.progress:row[9], EntityTable.c.kind:row[10], \
-                               EntityTable.c.extra_fields:row[11], EntityTable.c.dataset_id:row[12], EntityTable.c.user_id:row[13]})
+                               EntityTable.c.start_timestamp:row[13], EntityTable.c.end_timestamp:row[6], EntityTable.c.time_spent:row[7], EntityTable.c.progress:row[8], EntityTable.c.kind:row[9], \
+                               EntityTable.c.extra_fields:row[10], EntityTable.c.dataset_id:row[11], EntityTable.c.user_id:row[12]})
                         # logging.info(str(stmt))
-                        sinkConnection.execute(stmt)  
+                        sinkConnection.execute(stmt)
+
                 elif str(EntityTable.name) == 'kolibriauth_membership':
                     res = self.checkRecordExist(str(row[0]), EntityTable, sinkConnection, row)
                     if res != 0: 
@@ -114,11 +116,12 @@ class Fetcher(object):
                    if res != 0: 
                         # logging.info("inside else kolibriauth_collection") 
                         stmt = update(EntityTable).where(EntityTable.c.id==str(row[0])).values({EntityTable.c._morango_dirty_bit: row[1], \
-                               EntityTable.c._morango_source_id:row[2], EntityTable.c._morango_partition:row[3], EntityTable.c.item: row[4],EntityTable.c.start_timestamp:row[5] , \
-                               EntityTable.c.end_timestamp:row[6],EntityTable.c.completion_timestamp:row[7],EntityTable.c.time_spent:row[8], EntityTable.c.complete:row[9], EntityTable.c.correct:row[10], \
-                               EntityTable.c.hinted:row[11], EntityTable.c.answer:row[12], EntityTable.c.simple_answer:row[13],EntityTable.c.interaction_history:row[14],EntityTable.c.dataset_id:row[15], \
-                               EntityTable.c.masterylog_id:row[16], EntityTable.c.sessionlog_id:row[17], EntityTable.c.user_id:row[18] })
+                               EntityTable.c._morango_source_id:row[2], EntityTable.c._morango_partition:row[3], EntityTable.c.item: row[4],EntityTable.c.start_timestamp:row[18] , \
+                               EntityTable.c.end_timestamp:row[5],EntityTable.c.completion_timestamp:row[6],EntityTable.c.time_spent:row[7], EntityTable.c.complete:row[8], EntityTable.c.correct:row[9], \
+                               EntityTable.c.hinted:row[10], EntityTable.c.answer:row[11], EntityTable.c.simple_answer:row[12],EntityTable.c.interaction_history:row[13],EntityTable.c.dataset_id:row[14], \
+                               EntityTable.c.masterylog_id:row[15], EntityTable.c.sessionlog_id:row[16], EntityTable.c.user_id:row[17] })
                         # logging.info(str(stmt))
+                        
                         sinkConnection.execute(stmt)  
                 elif str(EntityTable.name) == 'logger_contentsummarylog':
                     res = self.checkRecordExist(str(row[0]), EntityTable, sinkConnection, row)
@@ -126,9 +129,11 @@ class Fetcher(object):
                         # logging.info("inside else kolibriauth_collection") 
                         stmt = update(EntityTable).where(EntityTable.c.id==str(row[0])).values({EntityTable.c._morango_dirty_bit: row[1], \
                                EntityTable.c._morango_source_id:row[2], EntityTable.c._morango_partition:row[3], EntityTable.c.content_id: row[4], EntityTable.c.channel_id:row[5], \
-                               EntityTable.c.start_timestamp:row[6], EntityTable.c.end_timestamp:row[7],EntityTable.c.completion_timestamp:row[8],EntityTable.c.time_spent:row[9], EntityTable.c.progress:row[10], EntityTable.c.kind:row[11], \
-                               EntityTable.c.extra_fields:row[12], EntityTable.c.dataset_id:row[13], EntityTable.c.user_id:row[14]})
+                               EntityTable.c.end_timestamp:row[6],EntityTable.c.completion_timestamp:row[7],EntityTable.c.time_spent:row[8], EntityTable.c.progress:row[9], EntityTable.c.kind:row[10], \
+                               EntityTable.c.extra_fields:row[11], EntityTable.c.dataset_id:row[12], EntityTable.c.user_id:row[13], EntityTable.c.start_timestamp:row[14]})
                         # logging.info(str(stmt))
+            
+                        
                         sinkConnection.execute(stmt)  
                 elif str(EntityTable.name) == 'logger_masterylog':
                     res = self.checkRecordExist(str(row[0]), EntityTable, sinkConnection, row)
@@ -136,9 +141,10 @@ class Fetcher(object):
                         # logging.info("inside else kolibriauth_collection") 
                         stmt = update(EntityTable).where(EntityTable.c.id==str(row[0])).values({EntityTable.c._morango_dirty_bit: row[1], \
                                EntityTable.c._morango_source_id:row[2], EntityTable.c._morango_partition:row[3], EntityTable.c.mastery_criterion: row[4], \
-                               EntityTable.c.start_timestamp:row[5], EntityTable.c.end_timestamp:row[6], EntityTable.c.completion_timestamp:row[7], EntityTable.c.mastery_level:row[8], EntityTable.c.complete:row[9], \
-                               EntityTable.c.dataset_id:row[10], EntityTable.c.summarylog_id:row[11], EntityTable.c.user_id:row[12]})
+                               EntityTable.c.start_timestamp:row[12], EntityTable.c.end_timestamp:row[5], EntityTable.c.completion_timestamp:row[6], EntityTable.c.mastery_level:row[7], EntityTable.c.complete:row[8], \
+                               EntityTable.c.dataset_id:row[9], EntityTable.c.summarylog_id:row[10], EntityTable.c.user_id:row[11]})
                         # logging.info(str(stmt))
+                        
                         sinkConnection.execute(stmt)
                 elif str(EntityTable.name) == 'logger_usersessionlog':
                     res = self.checkRecordExist(str(row[0]), EntityTable, sinkConnection, row)
@@ -146,10 +152,45 @@ class Fetcher(object):
                         # logging.info("inside else kolibriauth_collection") 
                         stmt = update(EntityTable).where(EntityTable.c.id==str(row[0])).values({EntityTable.c._morango_dirty_bit: row[1], \
                                EntityTable.c._morango_source_id:row[2], EntityTable.c._morango_partition:row[3], EntityTable.c.channels: row[4], \
-                               EntityTable.c.start_timestamp:row[5], EntityTable.c.last_interaction_timestamp:row[6], EntityTable.c.pages:row[7],\
-                               EntityTable.c.dataset_id:row[9], EntityTable.c.user_id:row[9]})
+                               EntityTable.c.start_timestamp:row[5], EntityTable.c.last_interaction_timestamp:row[9], EntityTable.c.pages:row[6],\
+                               EntityTable.c.dataset_id:row[7], EntityTable.c.user_id:row[8]})
                         # logging.info(str(stmt))
                         sinkConnection.execute(stmt)
+                elif str(EntityTable.name) == 'lessons_lesson':
+                    res = self.checkRecordExist(str(row[0]), EntityTable, sinkConnection, row)
+                    if res != 0:
+                        # logging.info("inside else kolibriauth_collection") 
+                        stmt = update(EntityTable).where(EntityTable.c.id==str(row[0])).values({EntityTable.c._morango_dirty_bit: row[1], \
+                               EntityTable.c._morango_source_id:row[2], EntityTable.c._morango_partition:row[3], EntityTable.c.title: row[4], \
+                               EntityTable.c.description:row[5], EntityTable.c.resources:row[6], EntityTable.c.is_active:row[7],\
+                               EntityTable.c.collection_id:row[8], EntityTable.c.created_by_id:row[9], EntityTable.c.dataset_id:row[10], EntityTable.c.date_created:row[11]})
+                        # logging.info(str(stmt))
+                elif str(EntityTable.name) == 'exams_exam':
+                    res = self.checkRecordExist(str(row[0]), EntityTable, sinkConnection, row)
+                    if res != 0:
+                        # logging.info("inside else kolibriauth_collection") 
+                        stmt = update(EntityTable).where(EntityTable.c.id==str(row[0])).values({EntityTable.c._morango_dirty_bit: row[1], \
+                               EntityTable.c._morango_source_id:row[2], EntityTable.c._morango_partition:row[3], EntityTable.c.title: row[4], \
+                               EntityTable.c.channel_id:row[5], EntityTable.c.question_count:row[6],\
+                               EntityTable.c.question_sources:row[7], EntityTable.c.seed:row[8], EntityTable.c.active:row[9], EntityTable.c.archive:row[10], EntityTable.c.collection_id:row[11], EntityTable.c.creator_id:row[12], EntityTable.c.dataset_id:row[13]})
+                elif str(EntityTable.name) == 'logger_examlog':
+                    res = self.checkRecordExist(str(row[0]), EntityTable, sinkConnection, row)
+                    if res != 0:
+                        # logging.info("inside else kolibriauth_collection") 
+                        stmt = update(EntityTable).where(EntityTable.c.id==str(row[0])).values({EntityTable.c._morango_dirty_bit: row[1], \
+                               EntityTable.c._morango_source_id:row[2], EntityTable.c._morango_partition:row[3], EntityTable.c.closed: row[4], \
+                               EntityTable.c.dataset_id:row[5], EntityTable.c.exam_id:row[6], EntityTable.c.user_id:row[7],\
+                               EntityTable.c.completion_timestamp:row[8]})
+                elif str(EntityTable.name) == 'logger_examattemptlog':
+                    res = self.checkRecordExist(str(row[0]), EntityTable, sinkConnection, row)
+                    if res != 0:
+                        # logging.info("inside else kolibriauth_collection") 
+                        stmt = update(EntityTable).where(EntityTable.c.id==str(row[0])).values({EntityTable.c._morango_dirty_bit: row[1], \
+                               EntityTable.c._morango_source_id:row[2], EntityTable.c._morango_partition:row[3], EntityTable.c.item: row[4], \
+                               EntityTable.c.end_timestamp:row[5], EntityTable.c.completion_timestamp:row[6], EntityTable.c.time_spent:row[7],\
+                               EntityTable.c.complete:row[8], EntityTable.c.correct:row[9], EntityTable.c.hinted:row[10], EntityTable.c.answer:row[11], \
+                               EntityTable.c.simple_answer:row[12], EntityTable.c.interaction_history:row[13], EntityTable.c.content_id:row[14],\
+                               EntityTable.c.channel_id:row[15], EntityTable.c.dataset_id:row[16], EntityTable.c.examlog_id:row[17], EntityTable.c.user_id:row[18], EntityTable.c.start_timestamp:row[19] })
                 else:
                     sinkConnection.execute(EntityTable.insert(), row)
             
@@ -166,6 +207,7 @@ class Fetcher(object):
             raise
 
     def checkRecordExist(self, rowID, EntityTable, sinkConnection, row):
+        # print (row)
         select_st = select([EntityTable]).where(EntityTable.c.id == rowID)
         res = sinkConnection.execute(select_st)
         count = res.rowcount
